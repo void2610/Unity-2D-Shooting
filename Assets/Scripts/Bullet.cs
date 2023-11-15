c08dc57c9a56255192af91afe1973c7abbe05d77
void2610 <izumisan2610@gmail.com>
2023-11-15 15:30:50 +0900

    Initial commit

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private Vector3 createdPosition;
+    void Start()
+    {
+        createdPosition = this.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position += Vector3.up * 0.1f;
+
+
+        if (Vector3.Distance(createdPosition, this.transform.position) > 10)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            Destroy(other.gameObject);
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject bulletPrefab;
+    private const float SPEED = 0.08f;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(bulletPrefab, this.transform.position, Quaternion.identity);
+        }
+
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            this.transform.position += Vector3.up * SPEED;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            this.transform.position += Vector3.down * SPEED;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            this.transform.position += Vector3.left * SPEED;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            this.transform.position += Vector3.right * SPEED;
+        }
+    }
+}
